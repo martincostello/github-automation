d969633cbfb70264df06b30204df73f184c9d0e9
martincostello <martin@martincostello.com>
2023-05-22 12:00:37 +0100

    Add rebase workflow
    Add first attempt at a tool and workflow to automatically rebase repositories.

---
+++ src/Rebaser/Program.cs
@@ -0,0 +1,186 @@
+﻿// Copyright (c) Martin Costello, 2023. All rights reserved.
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Xml.Linq;
+using LibGit2Sharp;
+using NuGet.Versioning;
+
+using var repo = new Repository(args[0]);
+
+var author = repo.Config.BuildSignature(DateTimeOffset.Now);
+var identity = new Identity(author.Name, author.Email);
+
+var branch = repo.Head;
+var target = repo.Branches["main"];
+
+var options = new RebaseOptions();
+Branch? onto = null;
+
+var result = repo.Rebase.Start(branch, target, onto, identity, options);
+
+var repoPath = Path.Combine(repo.Info.Path, "..");
+
+try
+{
+    while (result.Status is not RebaseStatus.Complete)
+    {
+        foreach (var conflict in repo.Index.Conflicts)
+        {
+            var filePath = Path.GetFullPath(Path.Combine(repoPath, conflict.Ours.Path));
+            var fileName = Path.GetFileName(filePath);
+
+            bool resolvedConflict = false;
+
+            if (string.Equals(fileName, "global.json", StringComparison.Ordinal))
+            {
+                // TODO Handle conflicts in global.json
+            }
+            else if (string.Equals(fileName, "package.json", StringComparison.Ordinal))
+            {
+                // TODO Handle conflicts in package.json
+            }
+            else if (string.Equals(fileName, "package-lock.json", StringComparison.Ordinal))
+            {
+                resolvedConflict = await TryResolveNpmLockFileConflictsAsync(filePath);
+            }
+            else if (await TryResolveNuGetPackageConflictsAsync(filePath, conflict))
+            {
+                resolvedConflict = true;
+            }
+
+            if (resolvedConflict)
+            {
+                repo.Index.Add(conflict.Ours.Path);
+            }
+            else
+            {
+                Console.Error.WriteLine($"Unable to resolve merge conflict in {filePath}.");
+                return;
+            }
+        }
+
+        result = repo.Rebase.Continue(identity, options);
+    }
+}
+finally
+{
+    repo.Rebase.Abort();
+}
+
+static async Task<bool> TryResolveNpmLockFileConflictsAsync(string fileName)
+{
+    File.Delete(fileName);
+
+    var projectPath = Path.GetDirectoryName(fileName)!;
+
+    var startInfo = new ProcessStartInfo("npm", "install")
+    {
+        UseShellExecute = true,
+        WorkingDirectory = projectPath,
+    };
+
+    using var process = Process.Start(startInfo)!;
+    await process.WaitForExitAsync();
+
+    return process.ExitCode == 0;
+}
+
+static bool TryParseNuGetPackageVersion(string value, [NotNullWhen(true)] out NuGetVersion? version)
+{
+    if (TryParseXml(value, out var fragment) && IsPackageElement(fragment))
+    {
+        var versionString = fragment.Attribute("Version")?.Value;
+
+        if (NuGetVersion.TryParse(versionString, out var packageVersion))
+        {
+            version = packageVersion;
+            return true;
+        }
+    }
+
+    version = null;
+    return false;
+
+    static bool TryParseXml(string value, [NotNullWhen(true)] out XElement? fragment)
+    {
+        try
+        {
+            fragment = XDocument.Parse(value).Root;
+            return fragment is not null;
+        }
+        catch (Exception)
+        {
+            fragment = null;
+            return false;
+        }
+    }
+
+    static bool IsPackageElement(XElement element)
+        => element.Name == "PackageReference" || element.Name == "PackageVersion";
+}
+
+static async Task<bool> TryResolveNuGetPackageConflictsAsync(string fileName, Conflict conflict)
+{
+    const string TheirsMarker = "<<<<<<<";
+    const string MidpointMarker = "=======";
+    const string OursMarker = ">>>>>>>";
+
+    var lines = await File.ReadAllLinesAsync(fileName);
+
+    int conflicts = lines.Count((p) => p.StartsWith(TheirsMarker));
+
+    var merged = new List<string>();
+    int line = 0;
+
+    for (int i = 0; i < conflicts; i++)
+    {
+        int theirIndex = Array.FindIndex(lines, line, (p) => p.StartsWith(TheirsMarker));
+        int midpoint = Array.FindIndex(lines, line, (p) => p.StartsWith(MidpointMarker));
+        int ourIndex = Array.FindIndex(lines, line, (p) => p.StartsWith(OursMarker));
+
+        merged.AddRange(lines[line..theirIndex]);
+
+        var theirs = lines[(theirIndex + 1)..midpoint];
+        var ours = lines[(midpoint + 1)..ourIndex];
+
+        bool resolvedConflict = false;
+
+        if (theirs.Length == ours.Length)
+        {
+            for (int j = 0; j < theirs.Length; j++)
+            {
+                var theirLine = theirs[j];
+                var ourLine = ours[j];
+
+                if (TryParseNuGetPackageVersion(theirLine, out var theirPackageVersion) &&
+                    TryParseNuGetPackageVersion(ourLine, out var ourPackageVersion))
+                {
+                    // Take the package version with the highest version number
+                    if (theirPackageVersion.CompareTo(ourPackageVersion) > 0)
+                    {
+                        merged.Add(theirLine);
+                    }
+                    else
+                    {
+                        merged.Add(ourLine);
+                    }
+
+                    resolvedConflict = true;
+                }
+            }
+        }
+
+        if (!resolvedConflict)
+        {
+            return false;
+        }
+
+        line = ourIndex + 1;
+    }
+
+    merged.AddRange(lines[line..]);
+
+    await File.WriteAllLinesAsync(fileName, merged);
+    return true;
+}
