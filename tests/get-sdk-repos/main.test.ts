15f94af6ce2abc10d588d9c5d4ee793ba2e892b5
martincostello <martin@martincostello.com>
2023-08-20 11:47:54 +0100

    Add tests for get-sdk-repos
    Add unit tests for the get-sdk-repos action.

---
+++ tests/get-sdk-repos/main.test.ts
@@ -0,0 +1,50 @@
+// Copyright (c) Martin Costello, 2023. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+import * as core from '@actions/core';
+import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
+import { ActionFixture } from '../ActionFixture';
+import { run } from '../../src/get-sdk-repos/main';
+import { setup } from '../fixtures';
+
+describe('get-sdk-repos', () => {
+  describe.each([
+    ['custom-config', 'dotnet-vnext', 'main', 'false', 'martincostello/website'],
+    ['empty-custom-config', 'dotnet-vnext', 'main', 'false', 'martincostello/website'],
+    ['ignored', 'dotnet-vnext', 'main', 'false', 'martincostello/website'],
+    ['multiple', 'dotnet-vnext', '', 'false', ''],
+    ['single', 'dotnet-vnext', 'main', 'false', 'martincostello/website'],
+  ])(
+    '%s',
+    (name: string, branch: string, base: string, force: string, repository: string) => {
+      let fixture: ActionFixture;
+
+      beforeAll(async () => {
+        await setup(`get-sdk-repos/${name}`);
+        await setup('get-sdk-repos/member-repos');
+
+        fixture = new ActionFixture(run);
+        await fixture.run({
+          'base': base,
+          'branch': branch,
+          'force': force,
+          'github-token': 'fake-token',
+          'repository': repository,
+        });
+      });
+
+      afterAll(async () => {
+        await fixture?.destroy();
+      });
+
+      test('generates no errors', () => {
+        expect(core.error).toHaveBeenCalledTimes(0);
+        expect(core.setFailed).toHaveBeenCalledTimes(0);
+      });
+
+      test('outputs the correct repositories', () => {
+        expect(fixture.getOutput('updates')).toMatchSnapshot();
+      });
+    }
+  );
+});
