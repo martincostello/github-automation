940bda1bf5877dd2bb0d5c5653a11ba55cf9ac0c
martincostello <martin@martincostello.com>
2023-08-20 09:04:32 +0100

    Simplify fixtures
    Simplify the fixtures by putting all the HTTP request scenarios into a single JSON file.

--- tests/dotnet-upgrade-report/main.test.ts
+++ tests/dotnet-upgrade-report/main.test.ts
@@ -15,61 +15,19 @@ describe('dotnet-upgrade-report', () => {
     let fixture: ActionFixture;
 
     beforeAll(async () => {
-      await setup(`dotnet/${channel}/sdk-product-version`);
-      await setup('repos/dotnet/core/contents/main/releases-index');
-      await setup('repos/martincostello/github-automation/contents/main/.github/workflow-config');
+      await setup(`sdk-versions`);
+      await setup('workflow-config');
 
-      // No branch
-      await setup(`repos/martincostello/adventofcode/branch/${branch}`);
+      await setup('dotnet-upgrade-report/failing-build');
+      await setup('dotnet-upgrade-report/merge-conflicts');
+      await setup('dotnet-upgrade-report/no-branch');
+      await setup('dotnet-upgrade-report/no-global-json');
+      await setup('dotnet-upgrade-report/no-pull-request');
+      await setup('dotnet-upgrade-report/no-pull-request-for-ref');
+      await setup(`dotnet-upgrade-report/out-of-date-${branch}`);
+      await setup(`dotnet-upgrade-report/up-to-date-${branch}`);
 
-      // No global.json
-      await setup(`repos/martincostello/alexa-london-travel/branch/${branch}`);
-
-      // No pull request
-      await setup(`repos/martincostello/api/branch/${branch}`);
-      await setup('repos/martincostello/api/commits/8257951/pulls');
-      await setup(`repos/martincostello/api/contents/${branch}/global`);
-      await setup('repos/martincostello/api/repo');
-
-      // No pull request targeting the branch
-      await setup(`repos/martincostello/apple-fitness-workout-mapper/branch/${branch}`);
-      await setup('repos/martincostello/apple-fitness-workout-mapper/commits/aa895b3/pulls');
-      await setup(`repos/martincostello/apple-fitness-workout-mapper/contents/${branch}/global`);
-      await setup('repos/martincostello/apple-fitness-workout-mapper/repo');
-
-      // Failing build
-      await setup(`repos/martincostello/costellobot/branch/${branch}`);
-      await setup('repos/martincostello/costellobot/commits/3ca5b65/check-runs');
-      await setup('repos/martincostello/costellobot/commits/3ca5b65/pulls');
-      await setup(`repos/martincostello/costellobot/contents/${branch}/global`);
-      await setup('repos/martincostello/costellobot/pulls/583');
-      await setup('repos/martincostello/costellobot/repo');
-
-      // Merge conflicts
-      await setup(`repos/martincostello/dependabot-helper/branch/${branch}`);
-      await setup('repos/martincostello/dependabot-helper/commits/12655a7/check-runs');
-      await setup('repos/martincostello/dependabot-helper/commits/12655a7/pulls');
-      await setup(`repos/martincostello/dependabot-helper/contents/${branch}/global`);
-      await setup('repos/martincostello/dependabot-helper/pulls/607');
-      await setup('repos/martincostello/dependabot-helper/repo');
-
-      // Out-of-date SDK version
-      await setup(`repos/martincostello/sqllocaldb/branch/${branch}`);
-      await setup('repos/martincostello/sqllocaldb/commits/a6245b1/check-runs');
-      await setup('repos/martincostello/sqllocaldb/commits/a6245b1/pulls');
-      await setup(`repos/martincostello/sqllocaldb/contents/${branch}/global`);
-      await setup('repos/martincostello/sqllocaldb/pulls/653');
-      await setup('repos/martincostello/sqllocaldb/repo');
-
-      // Up-to-date
-      await setup(`repos/martincostello/website/branch/${branch}`);
-      await setup('repos/martincostello/website/commits/adae44d/check-runs');
-      await setup('repos/martincostello/website/commits/adae44d/pulls');
-      await setup(`repos/martincostello/website/contents/${branch}/global`);
-      await setup('repos/martincostello/website/pulls/1398');
-      await setup('repos/martincostello/website/repo');
-
-      await setup('update-gist');
+      await setup('dotnet-upgrade-report/update-gist');
 
       fixture = new ActionFixture(run);
       await fixture.run({
--- tests/fixtures.ts
+++ tests/fixtures.ts
@@ -5,6 +5,10 @@ import nock from 'nock';
 import { join } from 'path';
 import * as fs from 'fs';
 
+type Fixture = {
+  scenarios: Scenario[];
+};
+
 type Scenario = {
   basePath: string;
   method?: 'GET' | 'PATCH';
@@ -18,17 +22,19 @@ nock.disableNetConnect();
 export async function setup(name: string): Promise<void> {
   const fileName = join(__dirname, 'fixtures', `${name}.json`);
   const json = await fs.promises.readFile(fileName, 'utf8');
-  const scenario: Scenario = JSON.parse(json);
+  const fixture: Fixture = JSON.parse(json);
 
-  const scope = nock(scenario.basePath);
+  for (const scenario of fixture.scenarios) {
+    const scope = nock(scenario.basePath);
 
-  let interceptor;
+    let interceptor;
 
-  if (scenario.method === 'PATCH') {
-    interceptor = scope.patch(scenario.path);
-  } else {
-    interceptor = scope.get(scenario.path);
+    if (scenario.method === 'PATCH') {
+      interceptor = scope.patch(scenario.path);
+    } else {
+      interceptor = scope.get(scenario.path);
+    }
+
+    interceptor.reply(scenario.status ?? 200, scenario.response);
   }
-
-  interceptor.reply(scenario.status ?? 200, scenario.response);
 }
