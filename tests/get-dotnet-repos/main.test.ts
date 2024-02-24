e066eb8a2b3c7947ca1d7fb8709004fd9d2a8d06
martincostello <martin@martincostello.com>
2024-02-24 09:16:40 +0000

    Add action to get .NET repositories
    Add a new action that just gets .NET repositories a GitHub token was write access to.

---
+++ tests/get-dotnet-repos/main.test.ts
@@ -0,0 +1,42 @@
+// Copyright (c) Martin Costello, 2023. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+import * as core from '@actions/core';
+import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
+import { ActionFixture } from '../ActionFixture';
+import { run } from '../../src/get-dotnet-repos/main';
+import { setup } from '../fixtures';
+
+describe('get-dotnet-repos', () => {
+  describe.each([
+    ['multiple', 'main', ''],
+    ['multiple', 'main', 'martincostello/website'],
+  ])('%s on %s for repository "%s"', (name: string, branch: string, repository: string) => {
+    let fixture: ActionFixture;
+
+    beforeAll(async () => {
+      await setup(`get-dotnet-repos/${name}`);
+      await setup('get-dotnet-repos/member-repos');
+
+      fixture = new ActionFixture(run);
+      await fixture.run({
+        'branch': branch,
+        'github-token': 'fake-token',
+        'repository': repository,
+      });
+    });
+
+    afterAll(async () => {
+      await fixture?.destroy();
+    });
+
+    test('generates no errors', () => {
+      expect(core.error).toHaveBeenCalledTimes(0);
+      expect(core.setFailed).toHaveBeenCalledTimes(0);
+    });
+
+    test('outputs the correct repositories', () => {
+      expect(fixture.getOutput('repositories')).toMatchSnapshot();
+    });
+  });
+});
