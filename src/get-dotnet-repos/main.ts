5ed208137b32a60940d25f26129b3bc04d07dbff
martincostello <martin@martincostello.com>
2024-02-24 09:16:40 +0000

    Add action to get .NET repositories
    Add a new action that just gets .NET repositories a GitHub token was write access to.

---
+++ src/get-dotnet-repos/main.ts
@@ -0,0 +1,72 @@
+// Copyright (c) Martin Costello, 2023. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+import * as core from '@actions/core';
+import { getOctokit } from '@actions/github';
+import { Context } from '@actions/github/lib/context';
+import { handle } from '../shared/errors';
+import { getDotNetSdk, getReposForCurrentUser } from '../shared/github';
+
+type DotNetRepository = {
+  ref: string;
+  repo: string;
+};
+
+export async function run(): Promise<void> {
+  try {
+    const branch = core.getInput('branch', { required: false });
+    const token = core.getInput('github-token', { required: false });
+    const github = getOctokit(token);
+
+    const ref = branch;
+    const repositories = await getReposForCurrentUser(github, 'member');
+
+    let singleRepository = core.getInput('repository', { required: false });
+
+    if (singleRepository && !singleRepository.includes('/')) {
+      const context = new Context();
+      singleRepository = `${context.repo.owner}/${singleRepository}`;
+    }
+
+    const result: DotNetRepository[] = [];
+
+    for (const repository of repositories) {
+      const full_name = repository.full_name;
+
+      if (singleRepository && singleRepository !== full_name) {
+        core.debug(`Skipping repository ${full_name}.`);
+        continue;
+      }
+
+      const owner = repository.owner;
+      const repo = repository.repo;
+
+      try {
+        // eslint-disable-next-line no-console
+        console.log(`Fetching data for ${full_name}.`);
+
+        if (!(await getDotNetSdk(github, owner, repo, branch))) {
+          core.debug(`The ${branch} branch of ${full_name} does not exist or does not have a global.json file.`);
+          continue;
+        }
+
+        const config: DotNetRepository = {
+          ref,
+          repo: full_name,
+        };
+
+        result.push(config);
+      } catch (err) {
+        core.debug(`Failed to get data for ${full_name}: ${err}`);
+      }
+    }
+
+    core.setOutput('repositories', JSON.stringify(result));
+  } catch (error: any) {
+    handle(error);
+  }
+}
+
+if (require.main === module) {
+  run();
+}
