d92d143f4eb006819dc374461df280b35331d01c
Martin Costello <martin@martincostello.com>
2026-05-24 16:52:00 +0100

    Add GitHub token action (#1661)
    - Bump brace-expansion to 5.0.6.
    - Regenerate the contents of `dist`.
    - Add action that uses the costellobot GitHub token broker to get a GitHub token.

---
+++ src/get-github-token/main.ts
@@ -0,0 +1,63 @@
+// Copyright (c) Martin Costello, 2023. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+import * as core from '@actions/core';
+import { handle } from '../shared/errors';
+import { fetch, Headers } from 'undici';
+
+const tokenEndpoint = 'https://costellobot.martincostello.com/github-token';
+
+export async function run(): Promise<void> {
+  try {
+    const profile = core.getInput('profile-name', { required: true });
+
+    // Get the OIDC token for the current workflow run
+    const idToken = await core.getIDToken();
+
+    if (!idToken) {
+      throw new Error('Failed to get GitHub OIDC token.');
+    }
+
+    // Request a GitHub token from the broker using the OIDC token and profile name
+    const request: TokenRequest = {
+      profile,
+    };
+
+    const response = await fetch(tokenEndpoint, {
+      method: 'POST',
+      headers: new Headers({
+        'Authorization': `Bearer ${idToken}`,
+        'Content-Type': 'application/json',
+        'User-Agent': 'martincostello/github-automation',
+      }),
+      body: JSON.stringify(request),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to get GitHub token from broker. Status: ${response.status}, Body: ${await response.text()}`);
+    }
+
+    const githubToken = (await response.json()) as Partial<TokenResponse> | null;
+
+    if (typeof githubToken?.token !== 'string' || !githubToken.token) {
+      throw new Error('Failed to get GitHub token from broker. No token was returned.');
+    }
+
+    core.setSecret(githubToken.token);
+    core.setOutput('token', githubToken.token);
+  } catch (error) {
+    handle(error);
+  }
+}
+
+if (require.main === module) {
+  run();
+}
+
+type TokenRequest = {
+  profile: string;
+};
+
+type TokenResponse = {
+  token: string;
+};
